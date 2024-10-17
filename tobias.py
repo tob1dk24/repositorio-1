a9e5ccb02c1c6b4c4e500585b6f8b7a0abc24ebb
tob1dk24 <tobyxd0403@gmail.com>
2024-10-17 20:56:16 +0000

    patopato

---
+++ tobias.py
@@ -0,0 +1,11 @@
+nombre=¨tobias¨
+numero=3
+booleano=True
+booleano=False
+print(numero)
+print(3)
+edadDeTobias=15
+edadParaVotar=16
+puedeVotar= edadParaVotar<=edadDeTobias
+If puedeVotar:
+print(¨puedevotar¨)
---
+++ while.py
@@ -0,0 +1,6 @@
+condicion=True
+contador=0
+while condicion
+     contador=contador+1
+     condicion=contador<10
+     print(contador)
