787a8ce957a3b80220191b1d2703343692df2ae1
tob1dk24 <tobyxd0403@gmail.com>
2024-10-24 21:02:04 +0000

    .

---
+++ for.py
@@ -0,0 +1,8 @@
+empieza=1
+termina=11
+for contador in range (empieza,termina):
+       print(contador)
+booleano=20>=16
+booleano=not(1==1)
+print(booleano)
+
---
+++ master.py
@@ -0,0 +1,10 @@
+nombre=¨Tobias¨
+print(´ hola ´+nombre)
+string=nombre
+booleano=True of False
+contador1=0
+while contador1<20:
+    contador1=contador1+1
+    for contador2 in range (20):
+       if (contador1%2==0) and (contador2%2==0):
+           print(contador1,´  ´,contador2)
